719b85fe1bd74db4ca6222f9aadcf651bf26f764
seth23michel <seth23michel@gmail.com>
2022-04-25 21:10:04 -0400

    Update test-test.py

--- test-test.py
+++ test-test.py
@@ -1,2 +1,10 @@
+import easygui
+
 if __name__ == '__main__':
-    print('Hello World')
+   con = easygui.ynbox("Should I continue", 'Yes or No', ('Yes', 'No'))
+
+   if con:
+      easygui.msgbox(f'Hello World', title="Tester test")
+   else:
+      choice = easygui.buttonbox('Click on your favorite flavor.', 'Favorite Flavor', ('Chocolate', 'Vanilla', 'Strawberry'))
+      print(choice)
